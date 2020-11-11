d7d1a96090ff5526dc587785b96a59edfe7b4086
Nexif <nexif@protonmail.com>
2020-11-11 18:06:31 +0100

    pepe

--- prepSources.sh
+++ prepSources.sh
@@ -2,13 +2,13 @@
 Deb_Ver=$(cat /etc/os-release | grep "VERSION_ID" | awk -F "\"" '{print $2}')
 if [ $Deb_Ver = "9" ]; then
     sources(){
-        sed -i '$ a\\ndeb http://deb.debian.org/debian/ stretch main contrib\ndeb-src http://deb.debian.org/debian/ stretch main contrib' | tee /etc/apt/sources.list
+        sed -i '$ a\\ndeb http://deb.debian.org/debian/ stretch main contrib\ndeb-src http://deb.debian.org/debian/ stretch main contrib' /etc/apt/sources.list
     }
 fi
 
 if [ $Deb_Ver = "10" ]; then
     sources(){
-        sed '$ a\\ndeb http://deb.debian.org/debian buster main contrib\ndeb-src http://deb.debian.org/debian buster main contrib' | tee /etc/apt/sources.list
+        sed -i '$ a\\ndeb http://deb.debian.org/debian buster main contrib\ndeb-src http://deb.debian.org/debian buster main contrib' /etc/apt/sources.list
     }
 fi
 
