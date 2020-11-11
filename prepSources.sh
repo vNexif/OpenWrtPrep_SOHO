e0569b8be374a48fe5a1be7441cdd6d45f3dbbf0
Nexif <nexif@protonmail.com>
2020-11-11 17:43:17 +0100

    IfFi

--- prepSources.sh
+++ prepSources.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 $Deb_Ver = $(cat /etc/os-release | grep "VERSION_ID" | awk -F "\"" '{print $2}')
 
-if [ $Deb_Ver = "10"]; then
+if [ $Deb_Ver = "9"]; then
     sources(){
         sed -i '$ a\\ndeb http://deb.debian.org/debian/ stretch main contrib\ndeb-src http://deb.debian.org/debian/ stretch main contrib' /etc/apt/sources.list.orig > /etc/apt/sources.list
     }
-else
+fi
+
+if [ $Deb_Ver = "10"]; then
     sources(){
         sed -i '$ a\\ndeb http://deb.debian.org/debian buster main contrib\ndeb-src http://deb.debian.org/debian buster main contrib' /etc/apt/sources.list.orig > /etc/apt/sources.list
     }
+fi
 
 doUpdate(){
     apt update; apt dist-upgrade -y; reboot;
