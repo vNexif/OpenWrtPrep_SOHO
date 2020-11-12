513184d9ee39d819be0fd2d50fc7801d2098f7e7
Nexif <33202338+vNexif@users.noreply.github.com>
2020-11-12 16:43:16 +0100

    Update prepHeaders.sh

--- prepHeaders.sh
+++ prepHeaders.sh
@@ -8,11 +8,11 @@ getEssential(){
     apt install linux-headers-amd64 make sudo build-essential -y
 }
 
-createUsers(){
-    /usr/sbin/adduser --disabled-password --shell /sbin/nologin osboxes 
-    /usr/sbin/usermod -a -G sudo osboxes
-    echo ' osboxes ALL=(ALL)    ALL' >> /etc/sudoers
-}
+#createUsers(){
+#    /usr/sbin/adduser --disabled-password --shell /sbin/nologin osboxes 
+#    /usr/sbin/usermod -a -G sudo osboxes
+#    echo ' osboxes ALL=(ALL)    ALL' >> /etc/sudoers
+#}
 
 #check if user is equal to 0
 if [ "$EUID" -ne 0 ]; then
@@ -20,5 +20,4 @@ if [ "$EUID" -ne 0 ]; then
 fi
 if [ "$EUID" = 0 ]; then
     getEssential
-    createUsers
 fi
